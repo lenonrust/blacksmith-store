c580f2cbbf463b7b178b429b61fc868361414c88
Lenon Rust <lenon.rust@gmail.com>
2022-07-31 21:47:30 -0300

    Testando avalidador git hub

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/login', loginRoutes);
-
 app.use('/orders', ordersRoutes);
-
 app.use('/products', productsRoutes);
-
 app.use('/users', usersRoutes);
 
 app.use(errorMiddleware);
